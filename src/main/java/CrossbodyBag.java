7482b021d33503bd33d1576a87b1c212b3e67047
adelinewang3 <adeline.wang@mail.utoronto.ca>
2022-10-09 18:35:21 -0400

    Fixed toString code in CrossbodyBag
    Missed the word straps so I added that in.
    - Expected :Blue Crossbody Bag with 2 straps (0 / 4)
    - Actual   :Blue Crossbody Bag with 2 (0 / 4)

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -51,7 +51,7 @@ public class CrossbodyBag extends Bag{
 
     @Override
     public String toString() {
-        return super.getColor() + " Crossbody Bag with " + this.straps + " (" + super.getNumberOfContents() + " / " +
+        return super.getColor() + " Crossbody Bag with " + this.straps + " straps (" + super.getNumberOfContents() + " / " +
                 super.getCapacity() + ")";
     }
 }
