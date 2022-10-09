90ab6087e6eacb988a7953e31808705cab48bb7b
adelinewang3 <adeline.wang@mail.utoronto.ca>
2022-10-09 18:23:53 -0400

    Completed TODO in CrossbodyBag
    - created constructor
    - created getter method for getNumberOfStraps
    - created enhance method that increase capacity of the CrossbodyBag by 2
    - overridden toString

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -30,3 +30,28 @@
  *
  *       See the tests in test/CrossBodyBagTest.java for examples.
  */
+
+public class CrossbodyBag extends Bag{
+
+    private final int straps;
+
+    public CrossbodyBag(String color, int capacity, int straps){
+        super(color, capacity);
+        this.straps = straps;
+    }
+
+    public int getNumberOfStraps(){
+        return this.straps;
+    }
+
+    @Override
+    public void enhance() {
+        super.increaseCapacity(2);
+    }
+
+    @Override
+    public String toString() {
+        return super.getColor() + " Crossbody Bag with " + this.straps + " (" + super.getNumberOfContents() + " / " +
+                super.getCapacity() + ")";
+    }
+}
